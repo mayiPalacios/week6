922bf53050f7b13244bd17245ecf67b0e3068164
mayiPalacios <00018619@uca.edu.sv>
2023-03-18 16:39:07 -0600

    fix scss card general

--- src/models/interfaceGames.tsx
+++ src/models/interfaceGames.tsx
@@ -1,14 +1,26 @@
+import { platform } from "os";
+
 export interface Iresults {
     id:number,
     background_image:string,
+    name?:string
+}
+
+export interface InamePlatform{
+       name:string;
+}
+
+export interface Iplatforms{
+    platform:InamePlatform
+    id:number;
+}
+
+export interface Idetails{
+    id:number,
+    background_image:string,
     name?:string,
+    rating?:string,
+    description?:string,
+    platforms:Iplatforms[];
 }
 
-export interface Iplatforms extends Iresults{
-        id:number;
-        background_image:string,
-        name?:string,
-        rating?:string,
-        description?:string
-
-}
